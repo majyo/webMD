ca013e8442bad2818b7f819beee88446a78a4da5
lihw <MajyoArisu@outlook.com>
2018-05-10 23:45:48 +0800

    update jobList

---
+++ src/components/jobList/jobInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import MDButton from '../general/MDButton';
+export default {
+  name: 'JobInfo',
+  components: {
+    MDButton
+  },
+  data () {
+    return {
+      resverd: ""
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
