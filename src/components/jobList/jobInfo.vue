432c16ded15df10b9c870cb1f59b516bc7a0ca4c
lihw <MajyoArisu@outlook.com>
2018-05-11 22:13:16 +0800

    update jobList

--- src/components/jobList/jobInfo.vue
+++ src/components/jobList/jobInfo.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <table>
+      <tr>
+        <th>作业序号</th>
+        <th>作业名称</th>
+        <th>作业信息</th>
+        <th>操作</th>
+      </tr>
+      <tr>
+        <td>{{jobInfo.jobId}}</td>
+        <td>{{jobInfo.jobName}}</td>
+        <td>{{jobInfo.jobDetail}}</td>
+        <td>
+          <MDButton>执行计算</MDButton>
+          <MDButton>导出作业</MDButton>
+          <MDButton>删除作业</MDButton>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -12,7 +30,11 @@ export default {
   },
   data () {
     return {
-      resverd: ""
+      jobInfo: {
+        jobId: 0,
+        jobName: "",
+        jobDetail: ""
+      }
     }
   }
 }
